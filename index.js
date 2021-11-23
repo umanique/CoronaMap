a4c86002c707136be6667e41614853a8f08a51cd
Uman Ali <imailumanali@gmail.com>
2021-11-23 21:20:07 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,37 @@
+
+function updateMap() {
+
+    fetch("/data.json")
+        .then(response => response.json())
+        .then(resp => {
+            console.log(resp.data)
+
+            resp.data.forEach(element => {
+
+                latitude = element.latitude;
+                longitude = element.longitude;
+
+                cases = element.infected;
+                if (cases > 255) {
+                    color = "rgb(255, 0, 0)";
+                }
+                else {
+                    color = `rgb(${cases}, 0, 0)`;
+                }
+
+                //Mark on the Map
+                new mapboxgl.Marker({
+                    draggable: false,
+                    color: color
+                })
+                    .setLngLat([longitude, latitude])
+                    .addTo(map);
+
+            });
+
+
+        })
+
+}
+console.log("Updating map with realtime data")
+setInterval(updateMap,10000);
